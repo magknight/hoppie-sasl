982711ed07ef3235a0416270ab0b017ed767a433
Alex Beavil <alex@aeroniemi.com>
2020-07-03 12:40:43 +0100

    added the wrap function and datarefs, it now kinda works

--- acars/acarsBackground.lua
+++ acars/acarsBackground.lua
@@ -4,69 +4,69 @@ local connected = true
 local json = require "json"
 
 function pollAcarsResponse(url, result, notError, error)
-    if notError then
-        if result == "ok " then
-            return true
-        end
+	if notError then
+		if result == "ok " then
+			return true
+		end
 
-        if string.sub(result, 1, 3) == "ok " then
-            message = string.gsub(result, "^ok", "")
-            local messageTable = {}
-            for row in string.gmatch(message, " {.-}}") do
-                row = string.gsub(row, "{%d- ", "{")
-                from, type, packet = string.match(row, "{(%w-) (%w-) {(.-)}")
-                if packet ~= "" then
-                    addCommMessage(os.date("!%H%M"), type, nil, packet, from, true)
-                end
-            end
-        end
-    else
-        logError(error)
-    end
+		if string.sub(result, 1, 3) == "ok " then
+			message = string.gsub(result, "^ok", "")
+			local messageTable = {}
+			for row in string.gmatch(message, " {.-}}") do
+				row = string.gsub(row, "{%d- ", "{")
+				from, type, packet = string.match(row, "{(%w-) (%w-) {(.-)}")
+				if packet ~= "" then
+					addCommMessage(os.date("!%H%M"), type, nil, packet, from, true)
+				end
+			end
+		end
+	else
+		logError(error)
+	end
 end
 function pollAcars()
-    -- pollAcarsResponse("hoppie", "ok {LOVV cpdlc {/data2/15//WU/CONTACT @WIEN SOUTH CTR@ @133.80}}", true)
-    -- pollAcarsResponse("hoppie", "ok {EGLL cpdlc {/data2/1832//R/CLR TO @LEPA@ RWY @27R@ DEP @MAXIT1F@ INIT CLB @6000ft@ SQUAWK @2262@ WHEN RDY CALL FREQ @121.700@ IF UNABLE CALL VOICE}} {BAW22 telex {SYSTEM TEST A}} {BAW22 posreq {}} {BAW22 cpdlc {/data2/4112//AN/READY?} {BAW22 cpdlc {/data2/4112//AN/READY?}", true)
-    network.getAsync({type = "poll", from = get(flightNumber)}, pollAcarsResponse)
+	-- pollAcarsResponse("hoppie", "ok {LOVV cpdlc {/data2/15//WU/CONTACT @WIEN SOUTH CTR@ @133.80}}", true)
+	-- pollAcarsResponse("hoppie", "ok {EGLL cpdlc {/data2/1832//R/CLR TO @LEPA@ RWY @27R@ DEP @MAXIT1F@ INIT CLB @6000ft@ SQUAWK @2262@ WHEN RDY CALL FREQ @121.700@ IF UNABLE CALL VOICE}} {BAW22 telex {SYSTEM TEST A}} {BAW22 posreq {}} {BAW22 cpdlc {/data2/4112//AN/READY?} {BAW22 cpdlc {/data2/4112//AN/READY?}", true)
+	network.getAsync({type = "poll"}, pollAcarsResponse)
 end
 function pollForAcars()
-    if get(acarsActiveDataref) == 1 then
-        timerForPoll = timerForPoll - get(frameTime)
-        if timerForPoll < 0 then
-            timerForPoll = 20
-            pollAcars()
-        end
-    end
+	if get(acarsActiveDataref) == 1 then
+		timerForPoll = timerForPoll - get(frameTime)
+		if timerForPoll < 0 then
+			timerForPoll = 20
+			pollAcars()
+		end
+	end
 end
 local aliasFile = sasl.getAircraftPath() .. "/settings/acarsAliases.json"
 function loadAcarsAliases()
-    commAliases = json.readAndParse(aliasFile)
+	commAliases = json.readAndParse(aliasFile)
 end
 function handleUplinkedValues(tab)
-    if tab.vhfFrequency then
-        mfrScratchpads[1] = "UL " .. tab.vhfFrequency
-    end
-    if tab.speed then
-        uplinkValues["speed"] = tab.speed
-        uplinkValues["mach"] = false
-    end
-    if tab.initialClimbft then
-        uplinkValues["altitude"] = tab.initialClimbft
-    end
-    if tab.mach then
-        uplinkValues["speed"] = tab.mach
-        uplinkValues["mach"] = true
-    end
-    if tab.qnh then --! don't know what to do
-    end
+	if tab.vhfFrequency then
+		mfrScratchpads[1] = "UL " .. tab.vhfFrequency
+	end
+	if tab.speed then
+		uplinkValues["speed"] = tab.speed
+		uplinkValues["mach"] = false
+	end
+	if tab.initialClimbft then
+		uplinkValues["altitude"] = tab.initialClimbft
+	end
+	if tab.mach then
+		uplinkValues["speed"] = tab.mach
+		uplinkValues["mach"] = true
+	end
+	if tab.qnh then --! don't know what to do
+	end
 end
 setupM = false
 function update()
-    if setupM == false then
-        settingsSetup()
-        loadAcarsAliases()
-        acarsPopup:setPosition(50, 50, 500, 750)
-        setupM = true
-    end
-    pollForAcars()
+	if setupM == false then
+		settingsSetup()
+		loadAcarsAliases()
+		acarsPopup:setPosition(50, 50, 500, 750)
+		setupM = true
+	end
+	pollForAcars()
 end
--- acars/acarsDatarefs.lua
+++ acars/acarsDatarefs.lua
@@ -1,15 +1,15 @@
-callsignDataref = createDatarefS("aero787/general/flightNumber", "-------", false, false)
+callsignDataref = createDatarefS("cpdlc/callsign", "-------", false, false)
 flightNumber = callsignDataref
 -- -----------------------------------------------------------------------------
 -- ANCHOR COMM datarefs
 -- -----------------------------------------------------------------------------
-commHeaderId = createDatarefI("aero787/cockpit/comm/headerId", 3)
-commPageId = createDatarefI("aero787/cockpit/comm/pageId", 5)
-commLeftScratchpad = createDatarefS("aero787/cockpit/comm/leftScratchpad", "")
-commLeftScratchpadKeyboard = createDatarefI("aero787/cockpit/comm/leftScratchpadKeyboard", 0)
-acarsLogonCodeDataref = createDatarefS("aero787/acars/logonCode", "")
-acarsRemoteServerDataref = createDatarefS("aero787/acars/remoteServer", "")
-acarsEnabledDataref = createDatarefI("aero787/acars/enabled", 0)
-acarsActiveDataref = createDatarefI("aero787/acars/active", 0)
-use833Dataref = createDatarefI("aero787/acars/use833", 0)
+commHeaderId = createDatarefI("cpdlc/headerId", 3)
+commPageId = createDatarefI("cpdlc/pageId", 5)
+commLeftScratchpad = createDatarefS("cpdlc/scratchpad", "")
+commLeftScratchpadKeyboard = createDatarefI("cpdlc/scratchpadKeyboard", 0)
+acarsLogonCodeDataref = createDatarefS("cpdlc/logonCode", "")
+acarsRemoteServerDataref = createDatarefS("cpdlc/remoteServer", "http://www.hoppie.nl/acars/system/connect.html")
+acarsEnabledDataref = createDatarefI("cpdlc/enabled", 0)
+acarsActiveDataref = createDatarefI("cpdlc/active", 0)
+use833Dataref = createDatarefI("cpdlc/use833", 0)
 frameTime = globalPropertyf("sim/time/framerate_period")
